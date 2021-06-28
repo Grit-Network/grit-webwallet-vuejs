f4b2ea22301f74202544480d22f935d532096cad
ryanpgarde <ryanpgarde@gmail.com>
2021-06-28 23:31:16 +0800

    =Login Page

--- src/views/pages/profiles/LoginPage.vue
+++ src/views/pages/profiles/LoginPage.vue
@@ -1,5 +1,111 @@
 <template>
-    <div class="login-profile-wrapper">
+    <div class="login">
+        <img src="@/assets/images/background-logo.png" alt="" class="background-logo" />
+
+        <div class="login-form">
+            <img src="@/assets/images/logo.png" alt="" class="logo" />
+
+            <ValidationObserver v-slot="{ handleSubmit }" slim>
+                <form class="form" onsubmit="event.preventDefault()">
+                    <div class="form-title">
+                        {{ $t('login_to_symbol_account') }}
+                    </div>
+
+                    <div class="mb-1.5">
+                        <div class="form-label">{{ $t('profile_name') }}</div>
+                        <ValidationProvider v-slot="{ errors }" :name="$t('profile_name')" :rules="`profileExists:${profileNames}`" slim>
+                            <ErrorTooltip field-name="profile_name" :errors="errors">
+                                <input v-show="false" v-model="formItems.currentProfileName" />
+
+                                <AutoComplete
+                                    v-model="formItems.currentProfileName"
+                                    placeholder=" "
+                                    :class="['select-account', !profilesClassifiedByNetworkType ? 'un_click' : 'profile-name-input']"
+                                    :disabled="performingLogin"
+                                >
+                                    <div class="auto-complete-sub-container scroll">
+                                        <div class="tips-in-sub-container">
+                                            {{ $t(profilesClassifiedByNetworkType ? 'select_a_profile' : 'no_profiles_in_database') }}
+                                        </div>
+                                        <div v-if="profilesClassifiedByNetworkType">
+                                            <div v-for="pair in profilesClassifiedByNetworkType" :key="pair.networkType">
+                                                <div v-if="pair.profiles.length">
+                                                    <span class="network-type-head-title">{{ getNetworkTypeLabel(pair.networkType) }}</span>
+                                                </div>
+                                                <Option
+                                                    v-for="(profile, index) in pair.profiles"
+                                                    :key="`${profile.profileName}${index}`"
+                                                    :value="profile.profileName"
+                                                    :label="profile.profileName"
+                                                >
+                                                    <span>{{ profile.profileName }}</span>
+                                                </Option>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </AutoComplete>
+                            </ErrorTooltip>
+                        </ValidationProvider>
+                    </div>
+
+                    <div class="form-label">{{ $t('password') }}</div>
+
+                    <ValidationProvider v-slot="{ errors }" mode="passive" vid="password" :name="$t('password')" rules="required|min:8">
+                        <ErrorTooltip field-name="password" :errors="errors">
+                            <input
+                                v-model="formItems.password"
+                                v-focus
+                                :class="[!profilesClassifiedByNetworkType ? 'un_click' : '', 'form-input large']"
+                                :placeholder="$t('please_enter_your_account_password')"
+                                type="password"
+                                :disabled="!profilesClassifiedByNetworkType || performingLogin"
+                            />
+                        </ErrorTooltip>
+                    </ValidationProvider>
+
+                    <div class="mt-1 flex justify-between">
+                        <div>
+                            Hint:
+                            <span v-if="!!getPasswordHint().length" class="prompt pointer" @click="formItems.hasHint = !formItems.hasHint">
+                                {{ $t('password_hint') }}
+                            </span>
+                        </div>
+
+                        <span
+                            class="pointer create-profile"
+                            :class="{ disabled: performingLogin }"
+                            @click="
+                                if (!performingLogin) {
+                                    $router.push({
+                                        name: 'profiles.importProfile.importStrategy',
+                                    });
+                                }
+                            "
+                        >
+                            {{ $t('create_a_new_account') }}?
+                        </span>
+                    </div>
+                    <div v-if="formItems.hasHint && !!getPasswordHint().length" class="hint">
+                        {{ $t('password_hint') }}: {{ getPasswordHint() }}
+                    </div>
+
+                    <Button
+                        v-if="profilesClassifiedByNetworkType"
+                        class="button primary large w-full mt-3.5"
+                        :loading="performingLogin"
+                        html-type="submit"
+                        @click.stop="handleSubmit(submit)"
+                    >
+                        {{ $t('login') }}
+                    </Button>
+                    <div v-else class="pointer button" @click="$router.push({ name: 'profiles.importProfile.importStrategy' })">
+                        {{ $t('register') }}
+                    </div>
+                </form>
+            </ValidationObserver>
+        </div>
+    </div>
+    <!-- <div class="login-profile-wrapper relative">
         <VideoBackground class="video-background-section" :src="require('@/views/resources/videos/symbol_3d_rotate.mp4')">
             <div class="switch-language-container">
                 <button class="trigger-accountlink" @click="$router.push('offlineTransaction')">
@@ -131,13 +237,15 @@
             <span class="version-panel">{{ $t('version') }}: {{ packageVersion }}</span>
             <span class="copyright_label">{{ $t('copyright') }}</span>
         </VideoBackground>
-    </div>
+    </div> -->
 </template>
 
 <script lang="ts">
 import LoginPageTs from './LoginPageTs';
 export default class LoginPage extends LoginPageTs {}
 </script>
+
+<style scoped src="./LoginPage.css"></style>
 <style lang="less" scoped>
-@import './LoginPage.less';
+// @import './LoginPage.less';
 </style>
