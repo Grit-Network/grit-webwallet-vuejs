ddbe5d6c949b68f4d83531b94126c5bc1a5d445c
ryanpgarde <ryanpgarde@gmail.com>
2021-08-09 10:10:30 +0800

    fix build error in harvesting

--- src/views/pages/harvesting/HarvestedBlocksList/HarvestedBlocksList.vue
+++ src/views/pages/harvesting/HarvestedBlocksList/HarvestedBlocksList.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="flex flex-col flex-grow">
-        <Table
+        <!-- <Table
             :loading="isFetchingHarvestedBlocks"
             :columns="columns"
             :data="blockList"
             :no-data-text="$t('no_harvested_blocks_yet')"
             class="table"
-        />
+        /> -->
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>{{ this.$t('block') }}</th>
+                    <th>{{ this.$t('fees_earned') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-if="blockList.length === 0">
+                    <td colspan="2" class="text-center">No data found</td>
+                </tr>
+
+                <template v-else>
+                    <tr v-for="(block, index) in blockList" :key="index">
+                        <td>{{ block.blockNo }}</td>
+                        <td>{{ block.fee }}</td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
         <div class="mt-2 mx-auto">
             <Pagination page-size="pageSize" :current="pageInfo.pageNumber" :last-page="pageInfo.isLastPage" @targetPage="onPageChange" />
         </div>
